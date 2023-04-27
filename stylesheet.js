49621627472ff08bcba3bfc28a09937ba85df1a8
Erich-VDB <85136309+Erich-VDB@users.noreply.github.com>
2023-04-27 07:52:33 -0700

    Add files via upload

---
+++ stylesheet.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph Deleted";  }
+function myFunctio() {
+    document.getElementById("dem").innerHTML = "Paragraph Deleted";  }
+
+
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+
+//this function gathers the input 
+function processForm(){
+    //make the div visible 
+    document.getElementById("userInfo").style.display = "";
+    //first name 
+    document.getElementById("firstName").innerText = document.getElementById("fname").value;
+
+}
